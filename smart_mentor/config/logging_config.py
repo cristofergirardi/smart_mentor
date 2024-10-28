c7341a0c10e0c6ef4c264735a105c9310905d9ff
cristofergirardi <cristofergirardi@gmail.com>
2024-10-28 20:14:05 -0300

    Creating gpt35cli

--- smart_mentor/config/logging_config.py
+++ smart_mentor/config/logging_config.py
@@ -2,5 +2,5 @@ import logging
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger('eldbot')
+    logger = logging.getLogger('smart_mentor')
     return logger
--- smart_mentor/modelos_api/openai/gpt35cli.py
+++ smart_mentor/modelos_api/openai/gpt35cli.py
@@ -1,16 +1,17 @@
 from langchain_openai import ChatOpenAI
+from ...model_objects.config_model import ConfigModel
 
 class GPT35Cli():
 
     def __init__(self) -> None:
         pass
 
-    def get_llm():
+    def get_llm(config: ConfigModel):
         llm = ChatOpenAI(
             model="gpt-3.5",
             temperature=0,
             max_tokens=None,
             timeout=None,
             max_retries=2,
-            api_key="..."            
+            api_key= config.openai_key            
         )
