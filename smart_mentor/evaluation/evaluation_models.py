208cdf936b51093e4d59f63250195969749b9f70
cristofergirardi <cristofergirardi@gmail.com>
2024-12-05 13:06:37 -0300

    Creating evaluation code

--- smart_mentor/evaluation/evaluation_models.py
+++ smart_mentor/evaluation/evaluation_models.py
@@ -8,6 +8,7 @@ import time
 import pandas as pd
 import random
 from typing import Final
+import json
 
 logger = logging_config.setup_logging()
 
@@ -22,16 +23,13 @@ class EvaluationModels():
         self.rouge = RougeEval()    
         logger.info("Evaluation Models is on!")
     
-    def get_prompt(self, question):
-        return self.orchestrator.prepare_prompt(question)
+    def get_prompt(self, question, hypothesis, **kwargs):
+        return self.orchestrator.prepare_prompt(question, hypothesis, **kwargs)
 
-    def get_response_openai(self, prompt):
+    def get_response_openai_by_prompt(self, prompt):
         return self.orchestrator.request_openai_by_prompt(prompt)
-    
-    def get_response_claudeai(self, question):
-        return self.orchestrator.request_claudeai(question)
 
-    def get_response_llama(self, prompt):
+    def get_response_llama_by_prompt(self, prompt):
         return self.orchestrator.request_llama_by_prompt(prompt)
 
     def get_metrics(self, hypothesis: str, model:str, orig_data: str, predict: str) -> list:        
@@ -48,10 +46,33 @@ class EvaluationModels():
         for new_row in new_rows:
             df_orig.loc[len(df_orig)] = new_row
         return df_orig
+        
+    def get_response(self, response:str):
+
+        string_search = '"program_created":'
+        index_string = response.find(string_search) + len(string_search)
+        new_response = response[index_string:]
+
+        if new_response.find('"""') == -1:
+            new_response = new_response.replace('"','',1)
+            if new_response.rfind('}') != -1:
+                index_string = new_response.rfind('}') - 2
+            else:
+                index_string = new_response.rfind(']') - 2
+            new_response = new_response[:index_string]
+        else:
+            new_response = new_response.replace('"""','',1)
+            if new_response.rfind('}') != -1:
+                index_string = new_response.rfind('}') - 2
+            else:
+                index_string = new_response.rfind(']') - 2
+            new_response = new_response[:index_string]
+
+        return new_response
     
     @property
     def generate_random_numbers(self):
-        random_numbers = [random.randint(0, 13109) for _ in range(10)]
+        random_numbers = [random.randint(0, 70) for _ in range(1)]
         return pd.DataFrame({'index': random_numbers})
 
 
@@ -62,7 +83,7 @@ if __name__ == "__main__":
     models = EvaluationModels(config)
 
     ## Creating file
-    file_random = "src/resources/random_numbers.csv"
+    file_random = "smart_mentor/resources/random_numbers.csv"
     df_indexes = pd.DataFrame()
     if not reader.checkFile(file_random):
         writer.write(file_random, models.generate_random_numbers)
@@ -70,15 +91,15 @@ if __name__ == "__main__":
     else:
         df_indexes = reader.readFile(file_random)
 
-    df = reader.readFile("src/resources/Python Programming Questions Dataset.csv")
+    df = reader.readFile("smart_mentor/resources/ground_truth_data.csv")
     df_selected = df.iloc[df_indexes['index']]
     df_metrics = pd.DataFrame(columns=models.COLUMNS_METRICS)
 
-    hypothese = "h0"
-    file_written = f"src/resources/Metrics_{hypothese}.csv"
+    hypothesis = "h0"
+    file_written = f"smart_mentor/resources/Metrics_{hypothesis}.csv"
     ## Creating file
     try:
-        reader.removeFile("src/resources/Metrics.csv")
+        reader.removeFile(file_written)
     except FileNotFoundError as e:
         logger.error("File does not found")
     
@@ -89,25 +110,80 @@ if __name__ == "__main__":
         logger.info(f"###### Counting {count} times ######")
         df_metrics = pd.DataFrame(columns=models.COLUMNS_METRICS)
         for row in df_selected.itertuples(): 
-
             user_question = ""
-
-            if row.Input is not None:
-                user_question = row.Instruction
+            if len(str(row._9).replace("Dicas&Dicas","")) > 0:
+                user_question = f"{row._2} \n {row._5} \n {row._6} \n {row._7} \n # Dica sobre a questão {row._9}"
             else:
-                user_question = f'{row.Instruction} \n Using this data like an input: {row.Input}'
+                user_question = f"{row._2} \n {row._5} \n {row._6} \n {row._7}"
+            reference = row._20
 
-            prompt = models.get_prompt(user_question)
+            if hypothesis == "h11":
+                logger.info("#### OPENAI")
+                new_prompt = models.get_prompt(hypothesis=hypothesis, question=user_question)            
+                response = "" 
+                for i in range(1,3):                
+                    response = models.get_response_openai_by_prompt(prompt=new_prompt)
+                    thought = i+1
+                    new_response = f'{user_question} \n {response}'
+                    new_prompt = models.get_prompt(hypothesis=hypothesis, question=new_response,thought=thought)
+                    time.sleep(60)
+                
+                logger.info(f"#### OPENAI response \n {response}") 
+                json_data = json.loads(response)
+                list_metrics = models.get_metrics(hypothesis=hypothesis,
+                                                  model="openai",
+                                                  orig_data=reference,
+                                                  predict=json_data["program_created"])
+                df_metrics = models.add_new_row(df_metrics, list_metrics)
 
-            response_openai = models.get_response_openai(prompt) 
+                logger.info("#### LLAMA")
+                new_prompt = models.get_prompt(hypothesis=hypothesis, question=user_question)
+                response = "" 
+                for i in range(1,3):               
+                    response = models.get_response_llama_by_prompt(prompt=new_prompt)
+                    thought = i+1
+                    new_response = f'{user_question} \n {response}'
+                    new_prompt = models.get_prompt(hypothesis=hypothesis, question=new_response,thought=thought)
+                    time.sleep(60)
 
-            list_metrics = models.get_metrics(hypothese, "openai", row.Output, response_openai)
-            df_metrics = models.add_new_row(df_metrics, list_metrics)
+                logger.info(f"#### LLAMA response \n {response}") 
+                try:
+                    json_data = json.loads(response)
+                    response = json_data["program_created"]
+                except Exception as e:
+                    response = models.get_response(response)
 
-            response_llama = models.get_response_llama(prompt)
+                list_metrics = models.get_metrics(hypothesis=hypothesis,
+                                                  model="llama", 
+                                                  orig_data=reference,
+                                                  predict=response)
+                df_metrics = models.add_new_row(df_metrics, list_metrics)
+            else:
+                ### Others hypotheses
+                new_prompt = models.get_prompt(hypothesis=hypothesis, question=user_question)
+                response = models.get_response_openai_by_prompt(prompt=new_prompt)
+                print(f"#### OPENAI \n {response}") 
+                json_data = json.loads(response)
+                list_metrics = models.get_metrics(hypothesis=hypothesis,
+                                                  model="openai",
+                                                  orig_data=reference,
+                                                  predict=json_data["program_created"])
+                for metrics in list_metrics:
+                    logger.info(f'From {metrics["metric"]} by rouge_score library -> Precision: {metrics["precision"]} Recall: {metrics["recall"]} fmeasure: {metrics["f1_score"]} ')
 
-            list_metrics = models.get_metrics(hypothese, "llama", row.Output, response_llama)
-            df_metrics = models.add_new_row(df_metrics, list_metrics)
+                response = models.get_response_llama_by_prompt(prompt=new_prompt)
+                print(f"#### LLAMA \n {response}") 
+                try:
+                    json_data = json.loads(response)
+                    response = json_data["program_created"]
+                except Exception as e:
+                    response = models.get_response(response)
+
+                list_metrics = models.get_metrics(hypothesis=hypothesis,
+                                                  model="llama", 
+                                                  orig_data=reference,
+                                                  predict=response)
+                df_metrics = models.add_new_row(df_metrics, list_metrics)
             
             logger.info(f'Finished the index {row.Index}')
             time.sleep(60)
