638bd839006d02ef69002a9260f216eed7b5fa9c
cristofergirardi <cristofergirardi@gmail.com>
2025-03-03 11:02:25 -0300

    Adding mediana, stdev and mean

--- smart_mentor/evaluation/evaluation_graphs.py
+++ smart_mentor/evaluation/evaluation_graphs.py
@@ -3,6 +3,7 @@ from ..file.smart_writer import SmartWriter
 from ..file.smart_reader import SmartReader
 import pandas as pd
 from scipy.stats import hmean
+import statistics as stats
 
 logger = logging_config.setup_logging()
 
@@ -19,11 +20,12 @@ if __name__ == "__main__":
     writer = SmartWriter()
     eg = EvaluationGraphs()
 
-    columns_metrics = ["h", "model", "metric", "similarity"]
+    columns_metrics = ["h", "model", "metric", "hmcmean", "mediana", "mean", "stdev"]
     hypotheses = ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12"]
-    metrics_name = ["bert", "codet5"]
+    # metrics_name = ["bert", "codet5"]
+    metrics_name = ["bert"]
 
-    file_written = f"smart_mentor/resources/Metrics_median_hypotheses.csv"
+    file_written = f"smart_mentor/resources/Metrics_hypotheses.csv"
     try:
         reader.removeFile(file_written)
     except FileNotFoundError as e:
@@ -46,9 +48,12 @@ if __name__ == "__main__":
                                             df_read['model'].str.contains(mode) &
                                             df_read['metric'].str.contains(metric)
                                             ).dropna(how='all')
-                    similarity = hmean(df_metrics['similarity'].apply(abs))
+                    hmc_mean = hmean(df_metrics['similarity'].apply(abs))
+                    mediana = stats.median(df_metrics['similarity'].apply(abs))
+                    mean = stats.mean(df_metrics['similarity'].apply(abs))
+                    stdev = stats.stdev(df_metrics['similarity'].apply(abs))
 
-                    new_list = [{"h": hypothese, "model": mode, "metric": metric, "similarity": similarity}]
+                    new_list = [{"h": hypothese, "model": mode, "metric": metric, "hmcmean": hmc_mean, "mediana": mediana, "mean": mean, "stdev": stdev}]
                     df_median = eg.add_new_row(df_median, new_list)
 
     ## Appending dataframe
